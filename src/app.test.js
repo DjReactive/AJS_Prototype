ec9cef71f54b3431863b50f57bdcbbf74e1076a5
Nikita <dragon768@mail.ru>
2021-12-09 21:07:14 +0500

    Task 1, Edit 2

--- src/app.test.js
+++ src/app.test.js
@@ -9,8 +9,10 @@ let zombie = new ch.Zombie('Zombie');
 let daemon = new ch.Daemon('Daemon');
 
 test('Character Class Error (No type) ', () => {
-  expect(new ch.Character('Daemon', 'Test')).toEqual(new Error ("Некорректные аргументы"));
+  const t = () => new ch.Character('Daemon', 'Test');
+  expect(t).toThrow(new Error ("Некорректные аргументы"));
 });
 test('Character Class Error (Invalid Name)', () => {
-  expect(new ch.Character('DaemonTestSword', 'Swordsman')).toEqual(new Error ("Некорректные аргументы"));
+  const t = () => new ch.Character('DaemonTestSword', 'Swordsman');
+  expect(t).toThrow(new Error ("Некорректные аргументы"));
 });
